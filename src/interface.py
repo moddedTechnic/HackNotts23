01767e1c12f0e2c2ebdb8e23b8e70236bbc85ebb
Diogo Centeno <d.ribeirocenteno@lancaster.ac.uk>
2023-02-11 14:16:46 +0000

    Added frequency time for value

--- src/interface.py
+++ src/interface.py
@@ -2,3 +2,9 @@
 def play(n: int) -> None:
     """Play a tone representing the number `n`, a value from 0x0-0xF"""
 
+    base_time = 1
+    high_time = (base_time/16) * n
+    low_time = base_time - high_time
+
+    #print(high_time)
+    #print(low_time)
