86b3f790c59e55709d95136809f2d354bc1e3ad3
Diogo Centeno <d.ribeirocenteno@lancaster.ac.uk>
2023-02-11 17:52:02 +0000

    Changed to hardcoded values for testing

--- src/data_player.py
+++ src/data_player.py
@@ -2,11 +2,15 @@ from interface import play
 from cryptography.fernet import Fernet
 
 def play_data(data, key):
+    
+    print(data)
 
-    sz_data = len(data).to_bytes(2, 'big').hex()
-    sz_key = len(key).to_bytes(2, 'big').hex()
+    sz_data = len(data.hex()).to_bytes(2, 'big').hex()
+    sz_key = len(key.hex()).to_bytes(2, 'big').hex()
 
     stream = sz_data + sz_key + data.hex() + key.hex()
+
+    print(stream)
 
     for n in stream:
         n = int(n, 16)
@@ -16,7 +20,8 @@ def play_data(data, key):
 if __name__ == '__main__':
 
     msg = b'beep'
-    key = Fernet.generate_key()
+    #key = Fernet.generate_key()
+    key = b'RtPOTIPFCPDD9kVrOJBYZVHZXnGr9XPbDAi5rD7GBE4='
     fernet = Fernet(key)
 
     encMsg = fernet.encrypt(msg)
