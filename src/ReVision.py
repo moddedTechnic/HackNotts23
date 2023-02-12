3a31a69cbbf72fbc0af233499b509c25682eab91
Jonathan Leeming <jpleeming51@gmail.com>
2023-02-12 01:49:59 +0000

    Fixed exception loop

--- src/ReVision.py
+++ src/ReVision.py
@@ -4,9 +4,6 @@ import DistanceSensor
 import Speaker
 
 def run_aid():
-    gpio.setmode(gpio.BOARD)
-    gpio.setwarning(False)
-
     sensor = DistanceSensor.DistanceSensor(11,12)
     sensor.run()
 
@@ -16,35 +13,35 @@ def run_aid():
     dist_range = [0.2, 1.0]
     speed_range = [0.2, 1.0]
 
-    while True:
-        speed = 0
-        dist = sensor.get_distance()
-        if dist < dist_range[0]:
-            speed = speed_range[0]
-        elif dist > dist_range[1]:
-            speed = speed_range[1]
-        else:
-            speed = dist
+    try:
+        while True:
+            speed = 0
+            dist = sensor.get_distance()
+            if dist < dist_range[0]:
+                speed = speed_range[0]
+            elif dist > dist_range[1]:
+                speed = speed_range[1]
+            else:
+                speed = dist
 
-        speaker.set_speed(speed)
-        print(f'Speed: {speed}')
-
-        try:
-            pass
-        except KeyboardInterrupt:
-            speaker.stop()
-            sensor.stop()
+            speaker.set_speed(speed)
+            print(f'Speed: {speed}')
+    except KeyboardInterrupt:
+        speaker.stop()
+        sensor.stop()
 
 
 def run():
-    
+    gpio.setmode(gpio.BOARD)
+    gpio.setwarnings(False)
+
     #create 2 speaker threads
     aid1 = threading.Thread(target=run_aid)
-    aid2 = threading.Thread(target=run_aid)
+    #aid2 = threading.Thread(target=run_aid)
 
     aid1.start()
-    aid2.start()
+    #aid2.start()
 
 if __name__ == '__main__':
-
     run()
+
