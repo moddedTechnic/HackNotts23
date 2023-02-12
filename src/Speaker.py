1a8fe3a4b350f8de9b9fb7272cfc3c99c3792631
Jonathan Leeming <jpleeming51@gmail.com>
2023-02-12 02:54:53 +0000

    Swapped play for buzz

--- src/Speaker.py
+++ src/Speaker.py
@@ -35,7 +35,7 @@ class Speaker(Runnable):
 
     def loop(self) -> None:
         while self.running:
-            self.play()
+            self.buzz()
 
     def buzz(self) -> None:
         self.click(220 * self.speed, self.speed)
